95e7bb9b2508d3931757b9ea8da964e32d527553
Shreyas Rai <shreyasrai23@gmail.com>
2020-12-09 00:28:29 -0800

    extracts URLs from a google collection dynamic webpage and stores them in a file

--- url_extractor.py
+++ url_extractor.py
@@ -1,9 +1,6 @@
 from bs4 import BeautifulSoup, SoupStrainer
-import urllib.request
-import re
 from selenium import webdriver
 import time
-import os
 
 URL = "https://www.google.com/collections/s/list/UxLbhnEFTYylpfNwzYqSiA/" \
       "tWCgSg5YIMY?q=AI-ML&sa=X&ved=2ahUKEwjFkMShnbvtAhVDVmMKHYaPAlAQ4r8DKAB6BAgAEAc"
@@ -30,48 +27,50 @@ def driver(browser):
 def dynamic_web_scrape(url):
     dr = driver('chrome')
     dr.get('https://stackoverflow.com')
-    # dr.find_element_by_name('identifier').send_keys('peeyush.rai@gmail.com')
     dr.find_element_by_xpath('/html/body/header/div/ol[2]/li[2]/a[1]').click()
-    time.sleep(3)
+    time.sleep(1)
     dr.implicitly_wait(5)
     dr.find_element_by_xpath('//*[@id="openid-buttons"]/button[1]').click()
-    time.sleep(3)
+    time.sleep(1)
     dr.find_element_by_name('identifier').send_keys('shreyasrai23@gmail.com')
     dr.find_element_by_xpath('//*[@id="identifierNext"]/div/button/div[2]').click()
-    dr.find_element_by_name('password').send_keys('') # Insert password here
-    time.sleep(3)
+    dr.find_element_by_name('password').send_keys('')  # Insert password here
+    time.sleep(1)
     dr.find_element_by_xpath('//*[@id="passwordNext"]/div/button/div[2]').click()
-    time.sleep(3)
+    time.sleep(1)
     dr.get(url)
     return dr.page_source
 
 
+def remove_alphanumeric(string):
+    return string[:string.find('&usg=')]
+
+
 def static_page_extract(html_source):
-    # html_page = urllib.request.urlopen("https://arstechnica.com")  # Static page
-    # url_ls = []
+    url_ls = []
     for link in BeautifulSoup(html_source, parse_only=SoupStrainer('a'), features="html.parser"):
         if link.has_attr('href'):
             url_str = link['href']
             if url_str[0:7] == "/url?q=":
-                # url_ls.append(url_str[7:])
-                print(url_str[7:])
-    # return url_ls
+                url_ls.append(remove_alphanumeric(url_str[7:]))
+    return url_ls
 
-#
-# def write_to_file(ls):
-#     """ Write the extracted text into a file for annotating"""
-#     file = open(r"urls.txt", "w")
-#     for i in range(len(ls)):
-#         file.write(ls[i])
-#     file.close()
+
+def write_to_file(ls):
+    """ Write the extracted text into a file for annotating"""
+    file = open(r"urls.txt", "w")
+    for i in range(len(ls)):
+        file.write(ls[i] + '\n')
+    file.close()
 
 
 def main():
     source = dynamic_web_scrape(URL)
     static_page_extract(source)
-    # url_ls = static_page_extract(source)
-    # write_to_file(url_ls)
+    url_ls = static_page_extract(source)
+    write_to_file(url_ls)
 
 
 if __name__ == '__main__':
     main()
+
