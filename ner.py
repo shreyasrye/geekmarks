7a7e125ad5230074dcbe786341670ba92a3232b2
Shreyas Rai <shreyasrai23@gmail.com>
2020-12-13 17:22:58 -0800

    compiles training data from the previously extracted metadata and trains an NER model with spacy and prodigy

--- ner.py
+++ ner.py
@@ -1,5 +1,6 @@
 import json 
 
+
 def write_train_data(read_file, write_file):
     """ Filter the metadata so only the headlines & urls are used for train data. """
     for line in open(read_file, 'r'):
@@ -10,7 +11,10 @@ def write_train_data(read_file, write_file):
             if key == 'headline':
                 text = value
             if key == 'publisher':
-                publisher = value
+                try:
+                    publisher = value['url']
+                except KeyError:
+                    publisher = value
         else:
             for key, value in dictionary.items():
                 # print('KEY:{}\nVALUE:{}'.format(key, value))
@@ -20,20 +24,18 @@ def write_train_data(read_file, write_file):
                             if x == 'headline':
                                 text = y
                             if x == 'publisher':
-                                publisher = y
-        output = f"{{'text':{text},'meta':{publisher}}}"
-        print(output)
-
-
-# # Recursive function to get the nested key value pairs in the jsons.
-# def get_nested_pair(key: str, dictionary: dict):
-#     print(type(dictionary))
-#     for k, value in dictionary.items():
-#         if k == key:
-#             return value
-#         if type(value == list):
-#             for d in value:
-#                 return get_nested_pair(key, json.loads(d))
+                                try:
+                                    publisher = y['url']
+                                except KeyError:
+                                    publisher = y
+        if text == "" and publisher == "":
+            continue
+        output = f'{{"text":{text},"meta":{publisher}}}'
+        output_file = open(write_file, 'a', encoding='utf-8')
+        output_file.write(output)
+        output_file.write("\n")
+        output_file.close()
+        # print(output)
 
 
 if __name__ == '__main__':
