cda31f65b274b53031e1f329435925d53df80156
shreyasrye <shreyasrai23@gmail.com>
2020-12-25 18:14:37 -0800

    config file for url_extractor.py

---
+++ config.py
@@ -0,0 +1,9 @@
+"""Insert your information for the corresponding values"""
+
+cfd = {
+    'google_usrnm': '',
+    'google_pass' : '',
+    'chromedriverpath' : '',
+    'operadriverpath' : '',
+    'edgedriverpath': ''
+    }
