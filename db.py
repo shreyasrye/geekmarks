8b0e062c85abc4c05a578298f719da35c2104b00
shreyasrye <shreyasrai23@gmail.com>
2021-05-13 03:21:58 -0400

    update database

--- db.py
+++ db.py
@@ -1,2 +1,29 @@
 import sqlite3
-db=sqlite3.connect('test')
+from sqlite3 import Error
+
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+
+
+if __name__ == "__main__":
+    sql_create_test_table = """ CREATE TABLE IF NOT EXISTS projects (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        begin_date text,
+                                        end_date text
+                                    ); """
+    
+    db=sqlite3.connect('test')
+    create_table(db, sql_create_test_table)
+
