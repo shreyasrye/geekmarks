1b78a965c8f93e329b3852074f1d518998846060
Shreyas Rai <shreyasrai23@gmail.com>
2021-06-19 18:15:33 -0700

    first iteration of the setup bash script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,15 @@
 # This bash script will setup a virtual environment and install all the dependencies using the 
-# requirements.txt
+pip install virtualenv
+virtualenv vpath
+
+# Activate the virtual environment
+case "$OSTYPE" in
+  darwin*)  source vpath/bin/activate ;; 
+  linux*)   source vpath/bin/activate ;;
+  msys*)    vpath\Scripts\activate ;;
+  *)        echo "unknown: $OSTYPE" ;;
+esac
+
+pip install -r requirements.txt
+
+deactivate
