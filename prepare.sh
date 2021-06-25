25722bac48b05799e963d4fac4a4fb46545397fc
shreyasrye <shreyasrai23@gmail.com>
2021-06-25 16:50:06 -0700

    renamed shell script

---
+++ prepare.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+pip install virtualenv
+virtualenv vpath
+
+# Activate the virtual environment
+case "$OSTYPE" in
+  darwin*)  source vpath/bin/activate ;; 
+  linux*)   source vpath/bin/activate ;;
+  msys*)    vpath\Scripts\activate ;;
+  *)        echo "unknown: $OSTYPE" ;;
+esac
+
+pip install -r requirements.txt
+python url_extractor.py
+
+deactivate
